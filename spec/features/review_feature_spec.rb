00cbf9be7623f5a62bb5acf1a225fa16eb377654
Camilla van Klinken <camillavk@gmail.com>
2014-11-05 12:21:06 +0000

    average rating displayed as stars

--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -40,7 +40,7 @@ describe 'reviewing' do
     click_link 'Sign out'
     sign_up('sam@example.com', 'testtest', 'testtest')
     leave_review('Great', '5')
-    expect(page).to have_content('Average rating: 4')
+    expect(page).to have_content('Average rating: ★★★★☆')
   end
 
 end
