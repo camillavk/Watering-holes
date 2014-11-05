47d77f3ad91054286d0316cc5bbe07b48888c047
Camilla van Klinken <camillavk@gmail.com>
2014-11-05 09:48:06 +0000

    customised validation messages

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ActiveRecord::Base
 
   has_many :reviews, dependent: :destroy
   belongs_to :user
-  validates :name, length: {minimum: 2}, uniqueness: true
+  validates :name, length: {minimum: 2}, uniqueness: {
+     :message => "This restaurant has already been created!" }
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
   validates :rating, inclusion: (1..5)
-  validates :user_id, uniqueness: true
+  validates :user_id, uniqueness: { 
+     :message => "You can't review a restaurant more than once!" }
 
 end
