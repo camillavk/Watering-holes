bc6b899e5205f479377133c85e79dc734351159a
Camilla van Klinken <camillavk@gmail.com>
2014-11-06 08:46:15 +0000

    facebook login set for heroku add

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,7 @@
 Devise.setup do |config|
 
   config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
